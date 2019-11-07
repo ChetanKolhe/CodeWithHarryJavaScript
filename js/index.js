5d606f2e8d6fc04199201cf0e0476e4387047a17
Chetan Kolhe <chetankolhe72@gmail.com>
2019-11-07 19:57:08 +0530

    This is first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.time("mycode")
+console.log("This is the log message where i can share the output")
+console.log(34)
+console.log([1,,2,3,4,4,5,5])
+console.log(true)
+console.log({a: "Cheta ",age : 98})
+console.table({a: "Cheta ",age : 98})
+console.error("This is the error message ")
+console.warn("This is the warning message ")
+// console.clear()
+ console.timeEnd("mycode")
+ 
---
+++ js/tut5.js
@@ -0,0 +1,4 @@
+console.log("This is new file");
+// Type conversion and type 
+let myVar = String(34);
+console.log(myVar,(typeof myVar));
