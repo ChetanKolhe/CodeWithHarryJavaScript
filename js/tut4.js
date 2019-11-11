4984fc341f6c5504d80dcf324829ef068de46fbf
Chetan Kolhe <chetankolhe72@gmail.com>
2019-11-11 16:36:49 +0530

    Completed lecture 4

---
+++ js/tut4.js
@@ -0,0 +1,46 @@
+// Primitive data types
+
+// String 
+let name = "chetan";
+console.log("Data string is " + name);
+console.log("Data type is " + (typeof name));
+
+// Number 
+let mark = 100;
+console.log("Data number is " + mark);
+console.log("Data type is " + (typeof mark));
+
+// Boolena
+let isDriver = false;
+console.log("Data boolen is " + isDriver);
+console.log("Data type is " + (typeof isDriver));
+
+// Null
+var let = null;
+console.log("Data type of null value " + typeof demo);
+
+// Reference data type
+
+// Array
+var arry = [1,2,3,4,"String"];
+console.log("Data type of null value " + typeof arry);
+
+// Json object
+var stDict = {
+    a:1,
+    b:2,
+    c:"demo"
+};
+console.log("Date type of null value "+ typeof stDict)
+
+// Function 
+let demoFunct = function demo(){};
+console.log("Datat type of fuction value is "+ typeof demoFunct);
+
+
+
+
+
+
+
+
