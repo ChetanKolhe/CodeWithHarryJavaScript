40768ce53a3a209f6553c6301bec18d3ec34f07f
Chetan Kolhe <chetankolhe72@gmail.com>
2019-11-13 18:00:34 +0530

    Day& extra

---
+++ tut8.js
@@ -0,0 +1,38 @@
+console.log("tut8.js");
+const age = 18;
+
+if (age == 19) {
+    console.log("Age is 19");
+} else if (age === 65){
+    console.log("Age is 65");
+}else {
+    console.log("Age is 15")
+}
+
+
+// Checking if varaible is defin or not 
+if (typeof vari == "undefined"){
+    console.log("Vari is not defined ");
+
+}else{
+    console.log("Varu is defined ");
+}
+
+
+// age = 18;
+// Ternory Operator
+console.log(age == 89 ? "Age is greater thatn ": "Age is not grean ");
+
+switch (age) {
+    case 18:
+        console.log("Age is 18");        
+        break;
+        
+    case 19:
+        console.log("Age is 18");        
+        break;
+    default:
+        break;
+}
+
+
