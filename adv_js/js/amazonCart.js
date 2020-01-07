fed667f2e1329c65bf66a6ff5062c12f52b026ee
Chetan Kolhe <chetankolhe72@gmail.com>
2020-01-07 12:24:37 +0530

    completed amazon project

--- adv_js/js/amazonCart.js
+++ adv_js/js/amazonCart.js
@@ -25,18 +25,30 @@ function purchaseItem(...fns) {
 }
 
 function addItemToCart(user, item) {
+    console.log("Add Item called")
     const updatedCart = user.cart.concat(item)
     return Object.assign({}, user, { cart: updatedCart });
 }
 
 function applyTaxToItem(user) {
-    return user;
+    console.log("Apply Tax")
+    const {cart} = user;
+    const taxRate = 1.3;
+    const updateCart = cart.map(item => {
+        return {
+            name : item.name,
+            price : item.price*taxRate
+        }
+    })
+
+    return Object.assign({},user,{cart:updateCart});
 }
 
 function buyItem(user) {
-    return user;
+    console.log("Purchase Item called ");
+    return Object.assign({},user,{purchase:user.cart});
 }
 
 function emptyCart(user) {
-    return user;
+    return Object.assign({},user,{cart:[]});
 }
